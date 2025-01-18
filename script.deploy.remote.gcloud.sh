62fecc806de6dbca221b8be63e062c6192df176c
Miguel Lemos <miguel_lemos@apple.com>
2025-01-18 21:54:57 +0100

    Init stack if not created already

--- script.deploy.remote.gcloud.sh
+++ script.deploy.remote.gcloud.sh
@@ -1,3 +1,9 @@
-pulumi stack init gcloud
+#!/bin/bash
+
+# Check if the stack exists
+if ! pulumi stack select gcloud 2>/dev/null; then
+    pulumi stack init gcloud
+fi
+
 pulumi stack select gcloud
 pulumi up
