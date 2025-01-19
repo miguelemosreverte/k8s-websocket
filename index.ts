f4a7671c28a0372b2d57e6341cb9de0230ce7191
Miguel Lemos <miguel_lemos@apple.com>
2025-01-19 04:36:14 +0100

    Attempting genesis

--- index.ts
+++ index.ts
@@ -1,137 +1,124 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as gcp from "@pulumi/gcp";
+import * as docker from "@pulumi/docker";
 import * as k8s from "@pulumi/kubernetes";
+import * as tf from "@pulumi/tf";
 
-const name = "helloworld";
+// Config
+const config = new pulumi.Config();
+const environment = config.get("environment") || "minikube";
+const isMinikube = environment === "minikube";
+const projectId = "development-test-02";
 
-// Set the region or zone
-const region = "us-central1"; // Change to your desired region
-const zone = "us-central1-a"; // Change to your desired zone
+// Create GKE cluster using official Terraform module if not using minikube
+let k8sProvider: k8s.Provider | undefined;
 
-// Get the latest GKE engine version
-const engineVersion = gcp.container
-  .getEngineVersions({ location: zone })
-  .then((v) => v.latestMasterVersion);
+if (!isMinikube) {
+  const gkeCluster = new tf.Resource("gke-cluster", {
+    type: "google",
+    source: "terraform-google-modules/kubernetes-engine/google",
+    version: "28.0.0",
+    properties: {
+      project_id: projectId,
+      name: "gke-cluster",
+      region: "us-central1",
+      network: "default",
+      subnetwork: "default",
 
-// Create a GKE cluster
-const cluster = new gcp.container.Cluster(name, {
-  initialNodeCount: 2,
-  minMasterVersion: engineVersion,
-  nodeVersion: engineVersion,
-  location: zone,
-  nodeConfig: {
-    machineType: "n1-standard-1",
-    oauthScopes: [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ],
-  },
-});
-
-// Export the Cluster name
-export const clusterName = cluster.name;
-
-// Manufacture a GKE-style kubeconfig
-export const kubeconfig = pulumi
-  .all([cluster.name, cluster.endpoint, cluster.masterAuth])
-  .apply(([name, endpoint, masterAuth]) => {
-    const context = `${gcp.config.project}_${zone}_${name}`;
-    return `apiVersion: v1
-clusters:
-- cluster:
-    certificate-authority-data: ${masterAuth.clusterCaCertificate}
-    server: https://${endpoint}
-  name: ${context}
-contexts:
-- context:
-    cluster: ${context}
-    user: ${context}
-  name: ${context}
-current-context: ${context}
-kind: Config
-preferences: {}
-users:
-- name: ${context}
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1beta1
-      command: gke-gcloud-auth-plugin
-      installHint: Install gke-gcloud-auth-plugin for use with kubectl by following
-        https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
-      provideClusterInfo: true
-`;
+      node_pools: [
+        {
+          name: "default-node-pool",
+          machine_type: "e2-standard-2",
+          min_count: 1,
+          max_count: 3,
+          disk_size_gb: 100,
+          disk_type: "pd-standard",
+          image_type: "COS_CONTAINERD",
+          auto_repair: true,
+          auto_upgrade: true,
+        },
+      ],
+    },
   });
 
-// Create a Kubernetes provider instance that uses our cluster from above
-const clusterProvider = new k8s.Provider(name, {
-  kubeconfig: kubeconfig,
+  // Create k8s provider from GKE cluster
+  k8sProvider = new k8s.Provider(
+    "gke-k8s",
+    {
+      kubeconfig: gkeCluster.properties.kubeconfig,
+    },
+    { dependsOn: [gkeCluster] },
+  );
+}
+
+// Registry setup for GCloud
+const registryServer = !isMinikube ? `gcr.io/${projectId}` : undefined;
+const registryUsername = !isMinikube ? "_json_key" : undefined;
+const registryPassword = !isMinikube
+  ? config.getSecret("registryPassword")
+  : undefined;
+
+// Image name setup
+const imageName = isMinikube ? "chat-app:v1" : `${registryServer}/chat-app:v1`;
+
+// Docker build
+const chatAppImage = new docker.Image("chat-app-image", {
+  build: {
+    context: ".",
+    platform: "linux/amd64",
+  },
+  imageName: imageName,
+  skipPush: isMinikube,
+  registry:
+    !isMinikube && registryServer
+      ? {
+          server: registryServer,
+          username: registryUsername,
+          password: registryPassword,
+        }
+      : undefined,
 });
 
-// Create a Kubernetes Namespace
-const ns = new k8s.core.v1.Namespace(name, {}, { provider: clusterProvider });
-
-// Export the Namespace name
-export const namespaceName = ns.metadata.apply((m) => m.name);
-
-// Create a NGINX Deployment
-const appLabels = { appClass: name };
+// Application deployment
+const appLabels = { app: "chat-app" };
 const deployment = new k8s.apps.v1.Deployment(
-  name,
+  "chat-app-deployment",
   {
-    metadata: {
-      namespace: namespaceName,
-      labels: appLabels,
-    },
+    metadata: { name: "chat-app" },
     spec: {
       replicas: 1,
       selector: { matchLabels: appLabels },
       template: {
-        metadata: {
-          labels: appLabels,
-        },
+        metadata: { labels: appLabels },
         spec: {
           containers: [
             {
-              name: name,
-              image: "nginx:latest",
-              ports: [{ name: "http", containerPort: 80 }],
+              name: "chat-app",
+              image: chatAppImage.imageName,
+              ports: [{ containerPort: 8080 }],
             },
           ],
         },
       },
     },
   },
-  {
-    provider: clusterProvider,
-  },
+  { provider: k8sProvider },
 );
 
-// Export the Deployment name
-export const deploymentName = deployment.metadata.apply((m) => m.name);
-
-// Create a LoadBalancer Service for the NGINX Deployment
+// Service
 const service = new k8s.core.v1.Service(
-  name,
+  "chat-app-service",
   {
-    metadata: {
-      labels: appLabels,
-      namespace: namespaceName,
-    },
+    metadata: { name: "chat-app" },
     spec: {
-      type: "LoadBalancer",
-      ports: [{ port: 80, targetPort: "http" }],
+      type: isMinikube ? "NodePort" : "LoadBalancer",
       selector: appLabels,
+      ports: [{ port: 8080, targetPort: 8080 }],
     },
   },
-  {
-    provider: clusterProvider,
-  },
+  { provider: k8sProvider },
 );
 
-// Export the Service name and public LoadBalancer endpoint
-export const serviceName = service.metadata.apply((m) => m.name);
-export const servicePublicIP = service.status.apply(
-  (s) => s.loadBalancer.ingress[0].ip,
-);
+// Exports
+export const deploymentName = deployment.metadata.name;
+export const serviceName = service.metadata.name;
+export const nodePort = isMinikube ? service.spec.ports[0].nodePort : undefined;
